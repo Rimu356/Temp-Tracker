27278319eafb432e5695fda459ff9a68d190a81b
Rimu356 <61114957@continental.edu.pe>
2025-04-30 09:24:09 -0500

    la otra parte faltante

---
+++ api-test.js
@@ -0,0 +1,11 @@
+const claveApi = '37a6afda56184eb698514938252404'; 
+const idioma = 'es';
+const ciudad = 'Huancayo';
+
+const apiClimaActual = `https://api.weatherapi.com/v1/current.json?q=${ciudad}&lang=${idioma}&key=${claveApi}`;
+
+const response = await fetch(apiClimaActual);
+let data = await response.json();
+
+console.log(data.location.localtime);
+
